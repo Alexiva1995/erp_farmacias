1e76184160e104f94717e484ed9d9c410bba23e2
yosleydyP <yosleydypena@valdusoft.com>
2025-06-24 08:56:39 -0400

    tabla productos

---
+++ resources/js/components/cards/QuotationProducts.vue
@@ -0,0 +1,53 @@
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: false,
+    default: "primary",
+  },
+  icon: {
+    type: String,
+    required: true,
+  },
+  stats: {
+    type: String,
+    required: true,
+  },
+  height: {
+    type: Number,
+    required: true,
+  },
+  series: {
+    type: Array,
+    required: true,
+  },
+  chartOptions: {
+    type: null,
+    required: true,
+  },
+});
+</script>
+
+<template>
+        <VCard>
+          <VCardItem>
+            <VCardTitle>Productos Cargados</VCardTitle>
+            <template #append>
+              <IconBtn
+                size="small"
+                :color="isCodeShown ? 'primary' : 'default'"
+                :class="isCodeShown ? '' : 'text-disabled'"
+                @click="isCodeShown = !isCodeShown"
+              >
+                <VIcon size="20" icon="tabler-code" />
+              </IconBtn>
+            </template>
+          </VCardItem>
+
+          <VCardText class="d-flex flex-column pb-0"> </VCardText>
+        </VCard>
+</template>
