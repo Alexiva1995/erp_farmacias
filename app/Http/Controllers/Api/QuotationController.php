b3b4fedb5e9b1b4f6f42ea88039b2f13f648b5a0
yosleydyP <yosleydypena@valdusoft.com>
2025-06-25 18:02:54 -0400

    controller

---
+++ app/Http/Controllers/Api/QuotationController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Services\Products\ProductQueryService;
+
+class QuotationController extends Controller
+{
+     public function __construct(
+        private ProductQueryService $productQueryService,
+    ) {
+    }
+
+   public function index(Request $request)
+    {
+        $query = $this->productQueryService->getFilteredQuery($request);
+        $perPage = $request->input('itemsPerPage', 10);
+
+        if ($perPage < 1) {
+            $items = $query->get();
+            return response()->json(['data' => $items, 'total' => $items->count()]);
+        }
+        $paginatedResult = $query->paginate($perPage);
+        return response()->json(['data' => $paginatedResult->items(), 'total' => $paginatedResult->total()]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
