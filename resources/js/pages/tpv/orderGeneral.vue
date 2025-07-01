6b0968225af38db44090ab1c00a0594628441088
yosleydyP <yosleydypena@valdusoft.com>
2025-07-01 08:08:56 -0400

    Agregando algunas tablas en pedido general

--- resources/js/pages/tpv/orderGeneral.vue
+++ resources/js/pages/tpv/orderGeneral.vue
@@ -1,3 +1,46 @@
 <script setup></script>
 
-<template></template>
+<template>
+<div>
+    <div class='mb-2'>
+    <OrderProductsTable
+      :products="products"
+      :loading="loading"
+      :total-product="totalProduct"
+      :items-per-page="itemsPerPage"
+      :page="page"
+      @update:options="updateTableOptions"
+    />
+    </div>
+     <div class='mb-2'>
+    <OrderProductsTable
+      :products="products"
+      :loading="loading"
+      :total-product="totalProduct"
+      :items-per-page="itemsPerPage"
+      :page="page"
+      @update:options="updateTableOptions"
+    />
+    </div>
+     <div class='mb-2'>
+    <OrderProductsTable
+      :products="products"
+      :loading="loading"
+      :total-product="totalProduct"
+      :items-per-page="itemsPerPage"
+      :page="page"
+      @update:options="updateTableOptions"
+    />
+      </div>
+        <div class='mb-2'>
+    <OrderProductsTable
+      :products="products"
+      :loading="loading"
+      :total-product="totalProduct"
+      :items-per-page="itemsPerPage"
+      :page="page"
+      @update:options="updateTableOptions"
+    />
+    </div>
+</div>
+</template>
