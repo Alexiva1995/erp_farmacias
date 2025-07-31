485ec99b22f5dbc4dccfda383a163fa0562538f4
Gabriel Valera <gabrielvalera@valdusoft.com>
2025-07-31 19:16:44 -0400

    save

--- resources/js/pages/suppliers/supplieriaorderassistant.vue
+++ resources/js/pages/suppliers/supplieriaorderassistant.vue
@@ -18,7 +18,7 @@ const modal= reactive({
 const tipo_de_vista= ref("");// grupo o individual
 const tipo_de_filtracion= ref("");// promedio o ventas
 const lapso_de_tiempo= ref("3 month");// tiempo
-const stock_exceso= ref("3 month");// Fallas , Execeso o All
+const stock= ref("3 month");// Fallas , Execeso o All
 </script>
 <template>
   <h1>UwU</h1>
