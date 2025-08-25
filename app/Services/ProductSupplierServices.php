2808d07d3d4df5ef4c25c4904598d0c3932df499
Gabriel Valera <gabrielvalera@valdusoft.com>
2025-08-25 11:06:31 -0400

    [fix] correcciones
    -  [x] Agrégame filtro por laboratorio (puedo seleccionar varios laboratorios) Solo me deja seleccionar uno
    -  [x] (consulta duda) El botón de generar pedido debe ser del color y estilo que todos los botones del sistema.
    -  [x] Ya cuando esta en el modal ponerle de titulo y mas grande en el color primario y en mayúscula IA ASSISTENCE
    -  [x] La primera pantalla “Productos con Costo Elevado” (en un badge el numero )
    -  [x] En el modal del generador de pedido se Debe de mostrar Proveedor(nombre), ID, Producto, Ventas, Promedio, Costo A. , Stock A, Costo P. , Sugerencia (este será el input)
    -  [x] La Segunda pantalla Productos con Costo Bajo (en un badge el numero )
    -  [x] la tercera Oportunidades de Mercado
    -  [x] Aquí es los productos que el coste estén mas bajo que su lote mas bajo en el ultimo año
    -  [x] En oportunidad de mercado agregar una columna que siga Costo Lot (este el costo del lote mas bajo y entre paréntesis la fecha que se creo)
    -  [x] Titulo de la pantalla de detalle (IA Detalle)
    -  [x] (consulta duda) En esta lista final puedo Editar la cantidad de un producto y también puedo borrar, un producto y también puedo borrar
    -  [x] (consulta duda) en la parte derecha donde esta el total que este cada proveedor con su total y al final un total
    -  [x] (consulta duda con el color) el botón debes de decir realizar compra que diga Generar y el modal tenga los colores y diseño de los modal del sistema y el botón diga Cancelar y el otro diga Confirmar

--- app/Services/ProductSupplierServices.php
+++ app/Services/ProductSupplierServices.php
@@ -8,6 +8,7 @@ use App\Models\ProductSupplier as ModelsProductSupplier;
 use App\Models\Supplier;
 use App\Repository\ProductLotsRepository;
 use App\Repository\ProductSupplierRepository;
+use DateTime;
 use Illuminate\Database\Eloquent\Collection;
 
 class ProductSupplierServices implements ProductSupplier
@@ -189,7 +190,10 @@ class ProductSupplierServices implements ProductSupplier
             $lote = $this->productLotsRepository->checkTheLotWithTheLowestPrice($producto["product"], $producto["supplier"]);
             if ($lote) {
                 if ((float)$producto["productSupplier"]->unit_cost < (float)$lote->unit_cost) {
+                    $dateLot = new DateTime($lote->created_at);
                     $producto["checkUniquePurchaseOpportunity"] = true;
+                    $producto["cost_lot"] = $lote->unit_cost;
+                    $producto["cost_lot_data"] = $dateLot->format("d-m-Y");
                     $productos[$index] = $producto;
                     $productosConOportunidad[] = $productos[$index];
                 }
