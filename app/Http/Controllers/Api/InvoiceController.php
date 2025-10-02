12a78f4fe2ce746c5e0668827e0f321119f452d7
LuisA-P <luisprieto@valdusoft.com>
2025-10-02 15:13:46 -0400

    Fix Register Facture

--- app/Http/Controllers/Api/InvoiceController.php
+++ app/Http/Controllers/Api/InvoiceController.php
@@ -66,8 +66,8 @@ class InvoiceController extends Controller
             $rules['exchange_rate'] = 'required|numeric|gt:0';
             $rules['total_usd'] = 'nullable|numeric|gt:0';
         } else {
-            $rules['exchange_rate'] = 'nullable|numeric|gt:0';
-            $rules['total_usd'] = 'nullable|numeric|gt:0';
+            $rules['exchange_rate'] = 'nullable|numeric';
+            $rules['total_usd'] = 'nullable|numeric';
         }
 
         $validator = Validator::make($request->all(), $rules);
