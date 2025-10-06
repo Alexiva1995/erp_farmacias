80d3eff4daad3ff8a729f8b000adfd457405436d
yosleydyP <yoslexio@gmail.com>
2025-10-06 09:23:34 -0400

    colocando de dos en dos los seller y colocando el porcentaje dependiendo de la cantidad de caracteres del seller

--- resources/js/components/dialogs/MonthlyCashModal.vue
+++ resources/js/components/dialogs/MonthlyCashModal.vue
@@ -145,6 +145,37 @@ const printReport = async () => {
     console.error("Error al visualizar el PDF:", error);
   }
 };
+
+const chunkArray = (array, size) => {
+    if (!array || !array.length) return [];
+    const chunkedArr = [];
+    for (let i = 0; i < array.length; i += size) {
+        chunkedArr.push(array.slice(i, i + size));
+    }
+    return chunkedArr;
+};
+
+const groupedSummary = computed(() => {
+    const filteredSummary = props.monthlyCashData.summary.filter(cashData => {
+        return parseFloat(cashData.total_sales) > 0;
+    });
+    return chunkArray(filteredSummary, 2);
+});
+
+const getDividerWidth = (name) => {
+    if (!name) return '40%';
+    const length = name.length;
+    if (length > 20) {
+        return '10%'; 
+    } else if (length > 15) {
+        return '30%'; 
+    } else if (length > 6){
+        return '35%'; 
+    } else {
+        return '40%'; 
+    }
+};
+
 </script>
 <template>
   <VDialog v-model="dialogVisible" max-width="700px">
@@ -160,102 +191,71 @@ const printReport = async () => {
       <VCardText>
         <div id="monthly-cash-report">
           <TicketHeader :logoSrc="BASE64_LOGO_DATA" />
-          <div class="container mt-3">
-            <div
-              :class="{
-                
-              }"
-            >
-              <div
-                class="row"
-                v-if="props.monthlyCashData.summary.length > 0"
-                :class="{
-                  'd-flex flex-wrap': props.monthlyCashData.summary.length > 1,
-                  'mb-4': true,
-                  'pdf-row-multi': props.monthlyCashData.summary.length > 1,
-                }"
-              >
-                <div
-                  v-for="(cashData, index) in props.monthlyCashData.summary"
-                  :key="index"
-                  :class="{
-                    'col-6 w-50': props.monthlyCashData.summary.length > 1,
-                    'col-12': props.monthlyCashData.summary.length === 1,
-                    'mb-4': true,
-                    'pdf-col-multi': props.monthlyCashData.summary.length > 1,
-                  }"
+<div class="container mt-3">
+    <table v-if="groupedSummary.length > 0" class="w-100">
+        <tbody>
+            <tr v-for="(pair, rowIndex) in groupedSummary" :key="rowIndex">
+                <td 
+                    v-for="(cashData, colIndex) in pair" 
+                    :key="colIndex"
+                    style="width: 50%; vertical-align: top; padding: 0;"
                 >
-                  <div class="w-100">
-                    <SectionDivider
-                      :isPdf="true"
-                      :text="cashData.seller_name"
-                      width="30%"
-                      class="center-block"
-                    />
+                    <div class="w-100">
+                        <SectionDivider
+                            :isPdf="true"
+                            :text="cashData.seller_name"
+                            :width="getDividerWidth(cashData.seller_name)"
+                            class="center-block"
+                        />
 
-                    <div v-if="cashData && cashData.total_sales !== '0.00'">
-                      <table
-                        class="table table-sm table-borderless"
-                        :class="{
-                          'w-75 mx-auto center-block': props.monthlyCashData.summary.length === 1,
-                          'w-100': props.monthlyCashData.summary.length > 1,
-                        }"
-                      >
-                        <tbody>
-                          <tr>
-                            <td class="text-left"><span>USD:</span></td>
-                            <td class="text-right">
-                              <span>{{ cashData.total_usd }}</span>
-                            </td>
-                            <td class="text-right">
-                              <span>{{ cashData.total_usd }}</span>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td class="text-left"><span>BS:</span></td>
-                            <td class="text-right">
-                              <span>{{ cashData.total_bs }}</span>
-                            </td>
-                            <td class="text-right">
-                              <span>{{ cashData.total_bs_in_usd }}</span>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td class="text-left"><span>COP:</span></td>
-                            <td class="text-right">
-                              <span>{{ cashData.total_cop }}</span>
-                            </td>
-                            <td class="text-right">
-                              <span>{{ cashData.total_cop_in_usd }}</span>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td class="text-left"><span></span></td>
-                            <td class="text-right fw-bold">
-                              <span>TOTAL VENTA</span>
-                            </td>
-                            <td class="text-right fw-bold">
-                              <span>{{ cashData.total_sales }}</span>
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
+                        <div v-if="cashData && cashData.total_sales !== '0.00'">
+                            <table
+                                class="table table-sm"
+                                style="width: 90%; margin-left: auto; margin-right: auto;"
+                            >
+                                <tbody>
+                                    <tr>
+                                        <td class="text-left"><span>USD:</span></td>
+                                        <td class="text-right"><span>{{ cashData.total_usd }}</span></td>
+                                        <td class="text-right"><span>{{ cashData.total_usd }}</span></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="text-left"><span>BS:</span></td>
+                                        <td class="text-right"><span>{{ cashData.total_bs }}</span></td>
+                                        <td class="text-right"><span>{{ cashData.total_bs_in_usd }}</span></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="text-left"><span>COP:</span></td>
+                                        <td class="text-right"><span>{{ cashData.total_cop }}</span></td>
+                                        <td class="text-right"><span>{{ cashData.total_cop_in_usd }}</span></td>
+                                    </tr>
+                                    <tr>
+                                        <td class="text-left"><span></span></td>
+                                        <td class="text-right fw-bold"><span>TOTAL VENTA</span></td>
+                                        <td class="text-right fw-bold">
+                                            <span>{{ cashData.total_sales }}</span>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
-                  </div>
-                </div>
-              </div>
-              <div
-                class="mt-3"
-              >
-                <SectionDivider
-                  :isPdf="true"
-                  text="TOTAL VENTA"
-                  width="35%"
-                  class="mx-auto center-block"
-                />
-                <div>
+                </td>
+                <td v-if="pair.length === 1" style="width: 50%; padding: 0;"></td>
+            </tr>
+        </tbody>
+    </table>
+    
+    <div class="mt-3">
+        <SectionDivider
+            :isPdf="true"
+            text="TOTAL VENTA"
+            width="35%"
+            class="mx-auto center-block"
+        />
+         <div>
                   <table
-                    class="table table-borderless table-sm w-75 mx-auto center-block"
+                    class="table table-sm w-75 mx-auto center-block"
                   >
                     <tbody>
                       <tr>
@@ -301,9 +301,12 @@ const printReport = async () => {
                     </tbody>
                   </table>
                 </div>
-              </div>
-            </div>
-          </div>
+
+        </div>
+</div>
+
+
+
         </div>
       </VCardText>
       <VCardActions class="p-2 d-flex justify-space-between w-100 mx-auto">
