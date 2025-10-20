5dccc3d971f60efc8e76548e0b754d84b5997afc
JesusMiguel03 <jmdefreitas10@gmail.com>
2025-10-20 15:54:10 -0400

    - añadir campo de exchange rate para tener la tasa del momento en que se registre - uso de exchange rate BS en vez de USD

--- app/Models/Payslip.php
+++ app/Models/Payslip.php
@@ -6,10 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payslip extends Model
 {
-    protected $fillable = ["payslip_date", "name", "status", "total"];
+    protected $fillable = ["payslip_date", "name", "status", "total", "exchange_rate_id"];
 
     public function details()
     {
         return $this->hasMany(PayslipDetails::class);
+    }
+
+    public function exchange_rate()
+    {
+        return $this->belongsTo(ExchangeRate::class);
     }
 }
