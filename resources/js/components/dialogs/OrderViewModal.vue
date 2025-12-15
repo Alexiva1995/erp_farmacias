c418fbb8dfe157c7719528f49f74965b235596cd
JesusMiguel03 <jmdefreitas10@gmail.com>
2025-12-15 13:09:04 -0400

    feat: añadir laboratorio al ver la order y chequear que al buscar por monto minimo devuelva los registros correctos

--- resources/js/components/dialogs/OrderViewModal.vue
+++ resources/js/components/dialogs/OrderViewModal.vue
@@ -234,6 +234,9 @@ const normalPayments = computed(() => {
               <VListItemTitle class="font-weight-medium me-4 mx-2">{{
                 product.title
               }}</VListItemTitle>
+              <VListItemSubtitle class="me-4 mx-2">
+                {{ product.laboratory }}
+              </VListItemSubtitle>
 
               <template #append>
                 <div class="d-flex align-center">
