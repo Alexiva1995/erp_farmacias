f985000f4b5d2650d843469fac4084576ccc149d
yosleydyP <yoslexio@gmail.com>
2026-01-19 10:33:55 -0400

    Nueva tabla para almancenar el conteo de los puntos de venta,

---
+++ app/Models/SaleCount.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class SaleCount extends Model
+{
+     use HasFactory;
+     protected $table = 'sales_counts';
+     protected $fillable = [
+        'cycle_id',
+        'product_id',
+        'counted_quantity',
+        'system_quantity',
+        'discrepancy',
+        'type',
+        'status',
+        'user_id',
+        'supervisor_id',
+    ];
+
+        public function cycle()
+    {
+        return $this->belongsTo(InventoryCycle::class, 'cycle_id');
+    }
+
+        public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
+        public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+       public function supervisor()
+    {
+        return $this->belongsTo(User::class, 'supervisor_id');
+    }
+
+    /**
+     * Un conteo puede tener muchas distribuciones en diferentes lotes.
+     */
+    public function distributions()
+    {
+        return $this->hasMany(SaleCountDistribution::class, 'sale_count_id');
+    }
+}
