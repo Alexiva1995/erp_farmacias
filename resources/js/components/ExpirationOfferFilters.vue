f13aafdeae54ebb70c13c541fb0f37b7ae4129e4
Alexis Jose Valera Valbuena <alexisjoseva95@gmail.com>
2026-02-11 10:11:21 -0400

    coomi

--- resources/js/components/ExpirationOfferFilters.vue
+++ resources/js/components/ExpirationOfferFilters.vue
@@ -33,7 +33,7 @@ const monthsOptions = [
 </script>
 
 <template>
-  <VCard title="Filtros" class="mb-6">
+  <VCard class="mb-6">
     <VCardText>
       <VRow>
         <VCol cols="12" sm="6" md="3">
