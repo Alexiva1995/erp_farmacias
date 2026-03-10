6e3ead03f63907a0654f96fc662f1d7ce087c647
Alexis valera <alexisjoseva95@gmail.com>
2026-03-10 15:07:36 -0400

    chore(bi): eliminar modulo y vistas businessIntelligence antiguas
    Se depuró todo rastro del anterior intento parcial de BI según las directivas, para favorecer exclusivamente la nueva arquitectura limpia en /bi

--- typed-router.d.ts
+++ typed-router.d.ts
@@ -22,7 +22,7 @@ declare module 'vue-router/auto-routes' {
     '$error': RouteRecordInfo<'$error', '/:error(.*)', { error: ParamValue<true> }, { error: ParamValue<false> }>,
     'auth-login': RouteRecordInfo<'auth-login', '/auth/login', Record<never, never>, Record<never, never>>,
     'balance-general': RouteRecordInfo<'balance-general', '/balance/general', Record<never, never>, Record<never, never>>,
-    'business-intelligence-report-abc': RouteRecordInfo<'business-intelligence-report-abc', '/businessIntelligence/reportAbc', Record<never, never>, Record<never, never>>,
+    'bi-report-abc': RouteRecordInfo<'bi-report-abc', '/bi/report-abc', Record<never, never>, Record<never, never>>,
     'configuration': RouteRecordInfo<'configuration', '/configuration', Record<never, never>, Record<never, never>>,
     'crm-clients': RouteRecordInfo<'crm-clients', '/crm/clients', Record<never, never>, Record<never, never>>,
     'crm-clients-pending': RouteRecordInfo<'crm-clients-pending', '/crm/clients/pending', Record<never, never>, Record<never, never>>,
