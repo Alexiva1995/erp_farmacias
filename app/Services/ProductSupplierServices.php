6cafa4fa1d7a1afd40f16b55ea20c4c90a9359f4
Alexis valera <alexisjoseva95@gmail.com>
2026-04-08 14:25:00 -0400

    fix(suppliers): añadir validación de nulos en checkTolerance y corregir sintaxis
    Se añadió un check para evitar el error 'Attempt to read property on null' cuando un producto no tiene ofertas. También se corrigió un error de sintaxis accidental en el servicio.

--- app/Services/ProductSupplierServices.php
+++ app/Services/ProductSupplierServices.php
@@ -135,15 +135,18 @@ class ProductSupplierServices implements ProductSupplier
 
             $replenishTheProduct = $replenishTheProducts[$index];
             $unitCostProductSupplier = 0;
-            if ($conDescuento == "true") {
-                if ($replenishTheProduct["productSupplier"]->unit_cost_usd_with_discount != null && $replenishTheProduct["productSupplier"]->unit_cost_usd_with_discount != "") {
-                    $replenishTheProduct["percentageIncrease"] = $this->calculatePercentageDifferenceIncrease($replenishTheProduct["product"]->unit_cost, $replenishTheProduct["productSupplier"]->unit_cost_usd_with_discount);
-                    $unitCostProductSupplier = (float) $replenishTheProduct["productSupplier"]->unit_cost_usd_with_discount;
-                }
-            } else {
-                if ($replenishTheProduct["productSupplier"]->unit_cost_usd != null && $replenishTheProduct["productSupplier"]->unit_cost_usd != "") {
-                    $replenishTheProduct["percentageIncrease"] = $this->calculatePercentageDifferenceIncrease($replenishTheProduct["product"]->unit_cost, $replenishTheProduct["productSupplier"]->unit_cost_usd);
-                    $unitCostProductSupplier = (float) $replenishTheProduct["productSupplier"]->unit_cost_usd;
+
+            if ($replenishTheProduct["productSupplier"]) {
+                if ($conDescuento == "true") {
+                    if ($replenishTheProduct["productSupplier"]->unit_cost_usd_with_discount != null && $replenishTheProduct["productSupplier"]->unit_cost_usd_with_discount != "") {
+                        $replenishTheProduct["percentageIncrease"] = $this->calculatePercentageDifferenceIncrease($replenishTheProduct["product"]->unit_cost, $replenishTheProduct["productSupplier"]->unit_cost_usd_with_discount);
+                        $unitCostProductSupplier = (float) $replenishTheProduct["productSupplier"]->unit_cost_usd_with_discount;
+                    }
+                } else {
+                    if ($replenishTheProduct["productSupplier"]->unit_cost_usd != null && $replenishTheProduct["productSupplier"]->unit_cost_usd != "") {
+                        $replenishTheProduct["percentageIncrease"] = $this->calculatePercentageDifferenceIncrease($replenishTheProduct["product"]->unit_cost, $replenishTheProduct["productSupplier"]->unit_cost_usd);
+                        $unitCostProductSupplier = (float) $replenishTheProduct["productSupplier"]->unit_cost_usd;
+                    }
                 }
             }
 
