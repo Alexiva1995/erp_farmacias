25295af6b994336305bc1dfdaa3fa3c6a950418e
Alexis valera <alexisjoseva95@gmail.com>
2026-05-07 19:49:12 -0400

    build(limpieza): remover scripts temporales, volcados de base de datos y archivos de prueba

--- test_data.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-
-$repo = new App\Repositories\ProductMasterReportRepository();
-$data = $repo->getPerformanceData(['start_date'=>'2026-04-01']);
-echo json_encode($data->sortByDesc('total_sold')->take(10)->values());
--- test_lab.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-
-$repo = new App\Repositories\ProductMasterReportRepository();
-$data = $repo->getLaboratoryRanking(['start_date'=>'2026-04-01']);
-echo json_encode($data);
