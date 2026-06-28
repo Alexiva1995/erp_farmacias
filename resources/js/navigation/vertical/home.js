59fecb9e7b561a8d1c8ce3e10722946e72e62c44
Alexis valera <alexisjoseva95@gmail.com>
2026-06-27 20:13:39 -0400

    fix(nav): cambiar propiedad 'to' de objeto a string en home.js para evitar errores en menu lateral

--- resources/js/navigation/vertical/home.js
+++ resources/js/navigation/vertical/home.js
@@ -2,14 +2,14 @@ export default [
   {
     title: 'Home',
     icon: { icon: 'tabler-home' },
-    to: { path: '/' },
+    to: 'index',
     action: 'manage',
     subject: 'admin',
   },
   {
     title: 'E-commerce',
     icon: { icon: 'tabler-shopping-cart' },
-    to: { path: '/tova-store' },
+    to: 'tova-store',
     action: 'manage',
     subject: 'admin',
   },
