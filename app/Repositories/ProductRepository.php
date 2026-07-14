e46c4b3c94cebe40ad69f2f247e2be90f36c8bb4
Alexis valera <alexisjoseva95@gmail.com>
2026-07-14 18:51:43 -0400

    fix(repositories): eliminar declaracion de interface en ProductRepository para evitar metodos abstractos no implementados

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -12,7 +12,7 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\DB;
 
-class ProductRepository implements \App\Contracts\Product
+class ProductRepository
 {
 
     private $subConsultaParaCalcularStockPorLotes = '(SELECT COALESCE (SUM(quantity), 0) 
