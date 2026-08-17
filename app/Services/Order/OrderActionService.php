974f3fd867ad3df020012f5adf8c03bce92db8f5
Alexis valera <alexisjoseva95@gmail.com>
2026-08-17 12:33:39 -0400

    fix(caja): registrar monto exacto de la venta en efectivo cop

--- app/Services/Order/OrderActionService.php
+++ app/Services/Order/OrderActionService.php
@@ -1327,7 +1327,13 @@ class OrderActionService
                             $current_cash->bs_card_credit += $amount;
                             break;
                         case 'cash_cop':
-                            $current_cash->cop_cash += $amount;
+                            if ($orderCurrency === 'COP') {
+                                // En ventas en COP se guarda estrictamente el valor total marcado en pantalla de la compra ($orderTotal)
+                                $copPortion = (count($request->payments) === 1 || $amount >= $orderTotal) ? $orderTotal : min($amount, $orderTotal);
+                                $current_cash->cop_cash += $copPortion;
+                            } else {
+                                $current_cash->cop_cash += $amount;
+                            }
                             break;
                         case 'bank_transfer':
                             $current_cash->cop_transfer += $amount;
@@ -1340,14 +1346,10 @@ class OrderActionService
             }
 
 
-            // Descontar el vuelto entregado en efectivo para registrar únicamente el neto ingresado en caja
-            $orderCurrency = strtoupper($orderId->currency);
-            $moneyReturns = (float) ($orderId->money_returns ?? 0);
-            $changeAmountUSD = (float) ($request->changeAmountUSD ?? 0);
-
+            // Descontar el vuelto entregado en efectivo para registrar únicamente el neto ingresado en caja en otras monedas
             if ($moneyReturns > 0 || $changeAmountUSD > 0) {
                 if ($orderCurrency === 'COP') {
-                    $current_cash->cop_cash -= $moneyReturns;
+                    // Para ventas en COP ya se acumuló el valor neto directo de la compra en cop_cash
                 } elseif ($orderCurrency === 'BS') {
                     $current_cash->bs_cash -= $moneyReturns;
                 } elseif ($orderCurrency === 'USD') {
