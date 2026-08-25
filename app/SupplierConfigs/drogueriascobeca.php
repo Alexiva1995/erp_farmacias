82580ec18d5002067d17fa74c8aff77249b62f88
Alexis valera <alexisjoseva95@gmail.com>
2026-08-25 14:24:58 -0400

    fix(proveedores): corregir endpoint y parametros de descarga API para Droguerias Cobeca

--- app/SupplierConfigs/drogueriascobeca.php
+++ app/SupplierConfigs/drogueriascobeca.php
@@ -3,20 +3,17 @@
 return [
     'productos' => function ($connection) {
         return [
-            'Drogueria' => 3,
+            'url' => 'https://comparadores.drogueriascobeca.com/api/Articulos',
+            'method' => 'post',
+            'payload' => [
+                'cod_drogueria' => 3,
+            ],
         ];
     },
     'facturas' => function ($connection) {
-        return [
-            'fechaInicio' => '2025-08-01T23:39:32.886Z',
-            'fechaFin' => now()->toIso8601String(),
-            'cliente' => 31373,
-            'drogueria' => 3,
-        ];
+        return [];
     },
     'factura_detalle' => function ($connection, $facturaId) {
-        return [
-            'url' => 'https://comparadores.drogueriascobeca.com/api/facturas/detalle?cod_factura=' . $facturaId,
-        ];
+        return [];
     },
 ];
