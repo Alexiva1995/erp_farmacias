02d503fede0b25f522955d78b3bc7d009a757aaf
Alexis valera <alexisjoseva95@gmail.com>
2026-09-16 22:09:30 -0400

    feat(finances): restaurar boton de procesar pago financiero individual en tabla de cuentas por pagar

--- resources/js/components/PendingPaymentTable.vue
+++ resources/js/components/PendingPaymentTable.vue
@@ -383,6 +383,17 @@ const openInvoiceTab = (item) => {
               <VTooltip activator="parent" location="top">Ver / Descargar PDF Digital</VTooltip>
             </IconBtn>
 
+            <!-- Procesar Pago (Modal Financiero) -->
+            <IconBtn
+              v-if="authStore.isAdmin"
+              color="success"
+              size="small"
+              @click="emit('process-payment', item)"
+            >
+              <VIcon icon="tabler-cash-register" size="18" />
+              <VTooltip activator="parent" location="top">Procesar Pago Financiero</VTooltip>
+            </IconBtn>
+
             <!-- Marcar como Pagado Directamente -->
             <IconBtn
               v-if="authStore.isAdmin"
@@ -548,6 +559,17 @@ const openInvoiceTab = (item) => {
                     </div>
                   </VCard>
                 </VMenu>
+
+                <!-- Procesar Pago (Móvil) -->
+                <IconBtn
+                  v-if="authStore.isAdmin"
+                  color="success"
+                  size="small"
+                  @click.stop="emit('process-payment', item)"
+                >
+                  <VIcon icon="tabler-cash-register" size="18" />
+                  <VTooltip activator="parent" location="top">Procesar Pago</VTooltip>
+                </IconBtn>
 
                 <!-- Marcar Pagada (Móvil) -->
                 <IconBtn
