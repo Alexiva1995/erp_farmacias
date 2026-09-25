2978433f9dbfba89f36ca3f0a1a872a647f8cbc5
Alexis valera <alexisjoseva95@gmail.com>
2026-09-25 15:25:07 -0400

    refactor(facturas): modularizar barra de cabecera con InvoiceHeaderBar

--- resources/js/pages/invoice/invoiceDetails.vue
+++ resources/js/pages/invoice/invoiceDetails.vue
@@ -8,6 +8,7 @@ import InvoiceFinancialSummary from "./components/InvoiceFinancialSummary.vue";
 import InvoiceAuditModal from "./components/InvoiceAuditModal.vue";
 import InvoicePdfSidePanel from "./components/InvoicePdfSidePanel.vue";
 import InvoiceMobileCards from "./components/InvoiceMobileCards.vue";
+import InvoiceHeaderBar from "./components/InvoiceHeaderBar.vue";
 import { useDisplay } from "vuetify";
 import axios from "@/plugins/axios";
 import { toast } from "@/plugins/sweetalert";
@@ -1625,99 +1626,20 @@ const detailsHeaders = computed(() => {
         >
           <VCard class="invoice-detail-card mb-6">
         <VForm @submit.prevent>
-          <VCardText class="header-section py-3 px-4">
-            <div class="d-flex flex-wrap align-center justify-space-between ga-3">
-              <!-- Bloque Izquierdo: Volver + Nombre Proveedor + Iconos PDF/Auditoría + Chips Fiscales -->
-              <div class="d-flex flex-wrap align-center ga-2 ga-sm-3">
-                <VBtn
-                  icon="tabler-arrow-left"
-                  variant="text"
-                  size="small"
-                  color="secondary"
-                  title="Volver a la lista"
-                  @click="emit('back-to-list')"
-                />
-                <div class="d-flex align-center ga-2">
-                  <span class="text-h5 font-weight-black text-primary text-uppercase">{{ invoice.supplier.name }}</span>
-                  <VTooltip text="Ver historial de auditoría">
-                    <template #activator="{ props }">
-                      <VBtn
-                        v-bind="props"
-                        icon="tabler-eye"
-                        size="x-small"
-                        color="secondary"
-                        variant="tonal"
-                        @click="showAuditModal = true"
-                      />
-                    </template>
-                  </VTooltip>
-                  <VTooltip v-if="invoice.invoice_photo" text="Ver documento original / PDF">
-                    <template #activator="{ props }">
-                      <VBtn
-                        v-bind="props"
-                        icon="tabler-file-type-pdf"
-                        size="x-small"
-                        color="error"
-                        variant="tonal"
-                        @click="viewInvoicePhoto(invoice.invoice_photo)"
-                      />
-                    </template>
-                  </VTooltip>
-                </div>
-                <div class="d-flex align-center ga-2 ms-sm-2">
-                  <VChip size="small" color="error" variant="tonal" class="font-weight-bold border">
-                    Control: {{ invoice.control_number || 'S/N' }}
-                  </VChip>
-                  <VChip size="small" color="primary" variant="tonal" class="font-weight-bold border">
-                    Factura: {{ invoice.invoice_number }}
-                  </VChip>
-                </div>
-              </div>
-
-              <!-- Bloque Derecho: Fechas + Modo Operativo + Botón Editar -->
-              <div class="d-flex flex-wrap align-center ga-2 justify-end">
-                <VChip size="small" variant="outlined" color="secondary" class="font-weight-medium">
-                  <VIcon start icon="tabler-calendar" size="13" />
-                  Emisión: {{ formatDate(invoice.created_invoice_date) || "N/A" }}
-                </VChip>
-                <VChip size="small" variant="outlined" color="info" class="font-weight-medium">
-                  <VIcon start icon="tabler-download" size="13" />
-                  Recibo: {{ formatDate(invoice.received_date) || "N/A" }}
-                </VChip>
-                <VChip
-                  size="small"
-                  :variant="isInvoiceDueSoon ? 'flat' : 'outlined'"
-                  :color="isInvoiceDueSoon ? 'error' : 'warning'"
-                  class="font-weight-bold"
-                >
-                  <VIcon start :icon="isInvoiceDueSoon ? 'tabler-alert-triangle' : 'tabler-calendar-due'" size="13" />
-                  Vence: {{ formatDate(invoice.payment_date || invoice.exp_date) || "N/A" }}
-                </VChip>
-
-                <VChip
-                  size="small"
-                  :color="isEditMode ? 'primary' : isApprovalMode ? 'warning' : isLocationMode ? 'info' : 'secondary'"
-                  variant="flat"
-                  class="font-weight-black uppercase"
-                >
-                  <VIcon start :icon="isEditMode ? 'tabler-edit' : isApprovalMode ? 'tabler-check' : isLocationMode ? 'tabler-map-pin' : 'tabler-eye'" size="13" />
-                  {{ isEditMode ? 'Edición' : isApprovalMode ? 'Aprobación' : isLocationMode ? 'Ubicación' : 'Lectura' }}
-                </VChip>
-
-                <VBtn
-                  v-if="isEditableMode && !isEditMode"
-                  color="primary"
-                  variant="flat"
-                  size="small"
-                  class="font-weight-bold ms-1"
-                  @click="toggleEditMode(true)"
-                >
-                  <VIcon icon="tabler-edit" class="me-1" size="15" />
-                  Editar Factura
-                </VBtn>
-              </div>
-            </div>
-          </VCardText>
+          <InvoiceHeaderBar
+            :invoice="invoice"
+            :is-pdf-side-panel-open="isPdfSidePanelOpen"
+            :is-invoice-due-soon="isInvoiceDueSoon"
+            :is-edit-mode="isEditMode"
+            :is-approval-mode="isApprovalMode"
+            :is-location-mode="isLocationMode"
+            :is-editable-mode="isEditableMode"
+            :format-date="formatDate"
+            @back-to-list="emit('back-to-list')"
+            @show-audit="showAuditModal = true"
+            @view-pdf="viewInvoicePhoto"
+            @toggle-edit="toggleEditMode"
+          />
           <VDivider />
 
           <!-- Alerta de Reglas de Pago Faltantes -->
